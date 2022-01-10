9ce6e3861c856bd1209189844f9558c37ba9a15c
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-01-10 13:27:43 +0530

    2

---
+++ Lecture/Conditionals and Loops/Largest_2.java
@@ -0,0 +1,16 @@
+package Lecture.Conditionals_Loops;
+
+import java.util.Scanner;
+
+public class Largest_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        int max = Math.max(c, Math.max(a, b));
+        
+        System.out.println(max);
+    }
+}
