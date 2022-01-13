cf72bf413d44ee685394093e4d5bb48cb544a5fd
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-01-13 15:34:18 +0530

    7

---
+++ Assignment-2/Intermediate/DepreciationOfValue.java
@@ -0,0 +1,21 @@
+package dsa_bootcamp.assignment2;
+
+import java.util.Scanner;
+
+public class DepreciationOfValue {
+    public static void main(String[] args) {
+        long amount,deppercent,year,temp;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter amount");
+        amount=sc.nextLong();
+        System.out.println("Enter Depreciation percentage");
+        deppercent=sc.nextLong();
+        System.out.println("Enter  number of years");
+        year=sc.nextLong();
+        temp=amount;
+        for(int i=0;i<year;i++){
+            temp=((100-deppercent)*temp)/100;
+        }
+        System.out.println("After depreciation = "+temp);
+    }
+}
