0d4ce31c32874d342e810cea271d8a69704b6c35
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-01-17 20:58:31 +0530

    5

---
+++ Lecture/Functions/Parameters_Integer.java
@@ -0,0 +1,11 @@
+package Lecture.Functions;
+public class Parameters_Integer {
+    public static void main(String[] args) {
+        int ans = sum(10,20);
+        System.out.println("Ans is: " + ans);
+    }
+    static int sum(int a, int b){
+        int sum = a +b;
+        return sum;
+    }
+}
