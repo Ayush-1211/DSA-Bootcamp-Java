786746775ef722c0a9abcb735d7ceda9778fa15f
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-01-27 19:24:19 +0530

    7

---
+++ Lecture/Arrays/Multidimension_ArrayList.java
@@ -0,0 +1,25 @@
+package Lecture.Arrays;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Multidimension_ArrayList {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        
+        // Initialisation
+        for(int i=0; i<3; i++){
+            list.add(new ArrayList<>());
+        }
+        
+        // add elements
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                list.get(i).add(sc.nextInt());
+            }
+        }
+        System.out.println(list);
+    }
+}
