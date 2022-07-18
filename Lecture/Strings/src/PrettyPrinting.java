865be1051410c008d8e755fe2f8c9a07b948e920
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-07-18 14:26:06 +0530

    4

---
+++ Lecture/Strings/src/PrettyPrinting.java
@@ -0,0 +1,12 @@
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.1274f;
+        System.out.printf("Formatted number is %.2f", a);   // printf = formatted print
+        System.out.println();
+
+        System.out.printf("PI %.3f", Math.PI);
+        System.out.println();
+
+        System.out.printf("Hello my name is %s and I am %s", "Ayush", "Cool");
+    }
+}
