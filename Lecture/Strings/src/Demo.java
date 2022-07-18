dd3485005791e660561e2c4e4876849e03e12737
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-07-18 14:24:55 +0530

    1

---
+++ Lecture/Strings/src/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+        String name = "Ayush Prajapati";
+        System.out.println(name);
+
+        String name2 = "Ayush";
+        String name3 = "Ayush";
+        System.out.println(name2);
+        System.out.println(name3);
+    }
+}
