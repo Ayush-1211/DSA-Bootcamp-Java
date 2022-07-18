b551b592d4b90af953e178c28a30dca123e20f37
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-07-18 14:26:31 +0530

    5

---
+++ Lecture/Strings/src/StringConcatenation.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class StringConcatenation {
+    public static void main(String[] args) {
+        System.out.println('a' + 'b');
+        System.out.println("a" + "b");
+        System.out.println((char)('a' + 3));
+
+        System.out.println("a" + 1);
+        // this is same as after a few steps: "a" + "1"
+        // int will be converted to Integer that will call toString()
+
+        System.out.println("Ayush" + new ArrayList<>());
+        System.out.println("Ayush" + new Integer(56));
+    }
+}
