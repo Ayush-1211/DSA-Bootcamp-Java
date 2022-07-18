b9b8a3eca4df5fa632fb93a7dbc5462ba43d3f35
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-07-18 14:25:50 +0530

    3

---
+++ Lecture/Strings/src/Output.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Output {
+    public static void main(String[] args) {
+        Integer num = new Integer(56);
+        System.out.println(num.toString());
+
+        System.out.println(Arrays.toString(new int[] {2, 3, 4, 5, 6}));
+    }
+}
