65ccfcc121b92916d5d529cf6ad30a4844d18aa5
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-07-21 13:33:42 +0530

    3

--- Lecture/Intro-to-Recursion/src/NumbersExampleRecursion.java
+++ Lecture/Intro-to-Recursion/src/NumbersExampleRecursion.java
@@ -11,6 +11,9 @@ public class NumbersExampleRecursion {
         System.out.println(n);
         // recursive call
         // if you are calling a function again and again, you can treat it as a separate call in the stack
+        
+        // this is called tail recursion
+        // this is the last function call
         print(n + 1);
     }
 }
