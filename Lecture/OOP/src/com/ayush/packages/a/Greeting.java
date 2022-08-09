ebb9d6a7dd61c162267489cb0b14c9ae4a359255
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-08-09 17:39:09 +0530

    Packages

---
+++ Lecture/OOP/src/com/ayush/packages/a/Greeting.java
@@ -0,0 +1,10 @@
+package com.ayush.packages.a;
+
+import static com.ayush.packages.b.Message.message;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        message();
+    }
+}
