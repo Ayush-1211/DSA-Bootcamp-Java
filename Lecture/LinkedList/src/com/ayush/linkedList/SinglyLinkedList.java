f1642c2ea194050459882b75aa7466a4ecf60fad
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-08-14 17:05:14 +0530

    LinkedList

--- Lecture/LinkedList/src/com/ayush/linkedList/SinglyLinkedList.java
+++ Lecture/LinkedList/src/com/ayush/linkedList/SinglyLinkedList.java
@@ -15,15 +15,14 @@ public class SinglyLinkedList {
         node.next = head;
         head = node;
 
-        if(tail == null) {
+        if (tail == null) {
             tail = head;
         }
-
         size += 1;
     }
 
     public void insertLast(int val) {
-        if(tail == null) {
+        if (tail == null) {
             insertFirst(val);
             return;
         }
@@ -34,35 +33,44 @@ public class SinglyLinkedList {
     }
 
     public void insert(int val, int index) {
-        if(index == 0) {
+        if (index == 0) {
             insertFirst(val);
             return;
         }
-        if(index == size) {
+        if (index == size) {
             insertLast(val);
             return;
         }
+
         Node temp = head;
-        for (int i=1; i<index; i++) {
+        for (int i = 1; i < index; i++) {
             temp = temp.next;
         }
+
         Node node = new Node(val, temp.next);
         temp.next = node;
+
         size++;
     }
 
-    public int deleteFirst() {
-        int val = head.value;
-        head = head.next;
-        if(head == null) {
-            tail = null;
+    // insert using recursion
+    public void insertRec(int val, int index) {
+        head = insertRec(val, index, head);
+    }
+    private Node insertRec(int val, int index, Node node) {
+        if (index == 0) {
+            Node temp = new Node(val, node);
+            size++;
+            return temp;
         }
-        size--;
-        return val;
+
+        node.next = insertRec(val, index-1, node.next);
+        return node;
     }
 
+
     public int deleteLast() {
-        if(size <= 1) {
+        if (size <= 1) {
             return deleteFirst();
         }
 
@@ -70,26 +78,30 @@ public class SinglyLinkedList {
         int val = tail.value;
         tail = secondLast;
         tail.next = null;
+        size--;
         return val;
     }
 
     public int delete(int index) {
-        if(index == 0) {
+        if (index == 0) {
             return deleteFirst();
         }
-        if(index == size - 1) {
+        if (index == size - 1) {
             return deleteLast();
         }
+
         Node prev = get(index - 1);
         int val = prev.next.value;
+
         prev.next = prev.next.next;
+        size--;
         return val;
     }
 
     public Node find(int value) {
         Node node = head;
         while (node != null) {
-            if(node.value == value) {
+            if (node.value == value) {
                 return node;
             }
             node = node.next;
@@ -99,10 +111,20 @@ public class SinglyLinkedList {
 
     public Node get(int index) {
         Node node = head;
-        for (int i=0; i<index; i++) {
+        for (int i = 0; i < index; i++) {
             node = node.next;
         }
         return node;
+    }
+
+    public int deleteFirst() {
+        int val = head.value;
+        head = head.next;
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        return val;
     }
 
     public void display() {
@@ -127,4 +149,150 @@ public class SinglyLinkedList {
             this.next = next;
         }
     }
+
+    // https://leetcode.com/problems/remove-duplicates-from-sorted-list
+    public void duplicates() {
+        Node node = head;
+
+        while (node.next != null) {
+            if (node.value == node.next.value) {
+                node.next = node.next.next;
+                size--;
+            } else {
+                node = node.next;
+            }
+        }
+        tail = node;
+        tail.next = null;
+    }
+
+    // https://leetcode.com/problems/merge-two-sorted-lists/submissions/
+    public static SinglyLinkedList merge(SinglyLinkedList first, SinglyLinkedList second) {
+        Node f = first.head;
+        Node s = second.head;
+
+        SinglyLinkedList ans = new SinglyLinkedList();
+
+        while (f != null && s != null) {
+            if (f.value < s.value) {
+                ans.insertLast(f.value);
+                f = f.next;
+            } else {
+                ans.insertLast(s.value);
+                s = s.next;
+            }
+        }
+
+        while (f != null) {
+            ans.insertLast(f.value);
+            f = f.next;
+        }
+
+        while (s != null) {
+            ans.insertLast(s.value);
+            s = s.next;
+        }
+
+        return ans;
+    }
+
+    public void bubbleSort() {
+        bubbleSort(size - 1, 0);
+    }
+
+    private void bubbleSort(int row, int col) {
+        if (row == 0) {
+            return;
+        }
+
+        if (col < row) {
+            Node first = get(col);
+            Node second = get(col + 1);
+
+            if (first.value > second.value) {
+                // swap
+                if (first == head) {
+                    head = second;
+                    first.next = second.next;
+                    second.next = first;
+                } else if (second == tail) {
+                    Node prev = get(col - 1);
+                    prev.next = second;
+                    tail = first;
+                    first.next = null;
+                    second.next = tail;
+                } else {
+                    Node prev = get(col - 1);
+                    prev.next = second;
+                    first.next = second.next;
+                    second.next = first;
+                }
+            }
+            bubbleSort(row, col + 1);
+        } else {
+            bubbleSort(row - 1, 0);
+        }
+    }
+
+    // recursion reverse
+    private void reverse(Node node) {
+        if (node == tail) {
+            head = tail;
+            return;
+        }
+        reverse(node.next);
+        tail.next = node;
+        tail = node;
+        tail.next = null;
+    }
+
+    // in place reversal of linked list
+    // google, microsoft, apple, amazon: https://leetcode.com/problems/reverse-linked-list/
+    public void reverse() {
+        if (size < 2) {
+            return;
+        }
+
+        Node prev = null;
+        Node present = head;
+        Node next = present.next;
+
+        while (present != null) {
+            present.next = prev;
+            prev = present;
+            present = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        head = prev;
+    }
+
+
+    public static void main(String[] args) {
+        SinglyLinkedList first = new SinglyLinkedList();
+        SinglyLinkedList second = new SinglyLinkedList();
+
+        first.insertLast(1);
+        first.insertLast(3);
+        first.insertLast(5);
+
+        second.insertLast(1);
+        second.insertLast(2);
+        second.insertLast(9);
+        second.insertLast(14);
+
+        SinglyLinkedList ans = SinglyLinkedList.merge(first, second);
+        ans.display();
+
+        SinglyLinkedList list = new SinglyLinkedList();
+        for (int i = 7; i > 0; i--) {
+            list.insertLast(i);
+        }
+        list.display();
+        list.bubbleSort();
+        list.display();
+
+    }
+
 }
