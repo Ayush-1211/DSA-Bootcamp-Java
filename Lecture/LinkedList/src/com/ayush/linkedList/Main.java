2c01784081e82663ff13d6ff86cff64782a2ca11
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2022-08-14 17:04:23 +0530

    Linked List

--- Lecture/LinkedList/src/com/ayush/linkedList/Main.java
+++ Lecture/LinkedList/src/com/ayush/linkedList/Main.java
@@ -2,22 +2,24 @@ package com.ayush.linkedList;
 
 public class Main {
     public static void main(String[] args) {
-//        SinglyLinkedList list = new SinglyLinkedList();
-//
-//        list.insertFirst(5);
-//        list.insertFirst(4);
-//        list.insertFirst(3);
-//        list.insertFirst(2);
-//        list.insertFirst(1);
-//        list.insertLast(99);
-//        list.insert(100, 4);
-//        list.display();
-//        System.out.println(list.deleteFirst());
-//        list.display();
-//        System.out.println(list.deleteLast());
-//        list.display();
-//        System.out.println(list.delete(2));
-//        list.display();
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.insertFirst(5);
+        list.insertFirst(4);
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(1);
+        list.insertLast(99);
+        list.insert(100, 4);
+        list.display();
+        System.out.println(list.deleteFirst());
+        list.display();
+        System.out.println(list.deleteLast());
+        list.display();
+        System.out.println(list.delete(2));
+        list.display();
+        list.insertRec(88, 2);
+        list.display();
 
 //        DoublyLinkedList list = new DoublyLinkedList();
 //
@@ -31,14 +33,14 @@ public class Main {
 //
 //        list.display();
 
-        CircularLinkedList list = new CircularLinkedList();
-        list.insert(1);
-        list.insert(2);
-        list.insert(3);
-        list.insert(4);
-        list.insert(5);
-        list.display();
-        list.delete(3);
-        list.display();
+//        CircularLinkedList list = new CircularLinkedList();
+//        list.insert(1);
+//        list.insert(2);
+//        list.insert(3);
+//        list.insert(4);
+//        list.insert(5);
+//        list.display();
+//        list.delete(3);
+//        list.display();
     }
 }
